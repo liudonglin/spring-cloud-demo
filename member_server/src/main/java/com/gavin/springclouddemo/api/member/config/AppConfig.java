3543d9866a0acfd33dd9a6f68967118d715d0d37
liudonglin <xiayangqiushi@hotmail.com>
2018-10-16 18:41:17 +0800

    add apollo client

---
+++ member_server/src/main/java/com/gavin/springclouddemo/api/member/config/AppConfig.java
@@ -0,0 +1,12 @@
+package com.gavin.springclouddemo.api.member.config;
+
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by gavin on 2018/10/16.
+ */
+@Configuration
+@EnableApolloConfig(value = "application", order = 10)
+public class AppConfig {
+}
--- member_server/src/main/java/com/gavin/springclouddemo/api/member/server/MemberBaseServer.java
+++ member_server/src/main/java/com/gavin/springclouddemo/api/member/server/MemberBaseServer.java
@@ -5,6 +5,8 @@ import com.gavin.springclouddemo.entity.OrderInfo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @Api(value = "会员服务", description = "与基础信息相关的服务", position = 1)
+@RefreshScope
 public class MemberBaseServer {
 
     @Autowired
@@ -33,6 +36,15 @@ public class MemberBaseServer {
     public String getSleepOrderInfo(){
         String orderInfo =  orderService.getOrderWithSleep();
         return orderInfo;
+    }
+
+    @Value("${config_info}")
+    private String config;
+
+    @ApiOperation(value = "测试apollo获取配置信息", notes = "测试apollo获取配置信息")
+    @RequestMapping("/getConfigInfo")
+    public String getConfigInfo(){
+        return config;
     }
 
 }
